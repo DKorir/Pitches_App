577e0f425207cd94ce828c5bf6a0d0ecc870c7e2
DKorir <dominic.korir@student.moringaschool.com>
2022-02-07 08:02:54 +0300

    registering blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import DevConfig
-from app import views
-from app import error
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+def create_app(config_name):
 
-from app import views
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
