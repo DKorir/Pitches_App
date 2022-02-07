92e83b51ee36add26ede25d691b9d6676b23b332
DKorir <dominic.korir@student.moringaschool.com>
2022-02-07 18:50:39 +0300

    import forms to register news users

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
