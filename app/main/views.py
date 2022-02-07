f10d162bf4dac6c04cec0616de6407ca8afb2aa6
DKorir <dominic.korir@student.moringaschool.com>
2022-02-07 20:14:55 +0300

    import abort function,then create a dynamic route for profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
+from ..models import User
 # Views
 @main.route('/')
 def index():
@@ -16,3 +17,11 @@ def index():
 @main.route('/login',methods=['GET','POST'])
 def login():
     return()
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
