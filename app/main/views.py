88bf4fa2607dbffcbb9742111fd5ff879d01cc89
DKorir <dominic.korir@student.moringaschool.com>
2022-02-07 07:36:22 +0300

    create a comment route

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,8 @@
 from flask import render_template
 from app import app
+from .models import comment
+from .forms import CommentForm
+Comment= comment.Comment
 # Views
 @app.route('/')
 def index():
@@ -10,3 +13,18 @@ def index():
 
     message = 'Hello World'
     return render_template('index.html',message = message)
+
+@app.route('/movie/comment/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
