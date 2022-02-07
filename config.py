3c5fa7f268dd3c385f657afa8b71e71302dfeb85
DKorir <dominic.korir@student.moringaschool.com>
2022-02-07 22:30:41 +0300

    setup smtp server with mail username and mail password

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ class Config:
     '''
     SECRET_KEY='secret_key'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("tommykiprono.db@gmail.com")
+    MAIL_PASSWORD = os.environ.get("T1234567-h")
+
 
 
 
